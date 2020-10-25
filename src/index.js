b51f8de1872a16be7c27d3a33cda256c0b576b53
Jouni Männistö <jouni.mannisto@gmail.com>
2020-10-25 19:22:46 +0200

    Data model updates

--- src/index.js
+++ src/index.js
@@ -29,25 +29,50 @@ io.on('connection', (socket) => {
 mqttClient.on("message", function (topic, message) {
   // Buffer-olio
   const mes = JSON.parse(message.toString()).VP;
-    //console.log(mes)
+  console.log(mes)
   const includesLocation = mes.lat && mes.long;
   // tehdään konvertointeja ja täällä: frontendin kuorma kevenee
-  if(includesLocation){
+  // mes.length < 3 <- tietomäärää pienemmäksi jätetään bussiliikennettä pois
+  if(includesLocation && mes.desi.length < 3){
     const vehicleDTO = {
-      speed: Math.round(mes.spd * 3.6), // m/s -> km/h
+        id: `${mes.oper}${mes.veh}`,
+        speed: Math.round(mes.spd * 3.6), // m/s -> km/h
       location: { lat: mes.lat, lon: mes.long },
-      routeNumber: mes.desi,
+      route: mes.desi,
       veh: mes.veh, // dokumentaation mukaan "veh" joillakin operaattoireilla päällekkäisiä numeroita > 
-      dl: mes.dl,
+      dl: Math.round( mes.dl/60) , // s -> min
+      operatorId: mes.oper,
+      operatorName: operators.find( o => o.id === mes.oper).name
     };
-    console.log(vehicleDTO)
+    //console.log(vehicleDTO)
     io.emit(VEHICLE_EVENT, vehicleDTO);
   }
-  
-
-  
 });
 
 server.listen(3000, () => {
   console.log("listening on *:3000");
 });
+
+const operators = [
+  { id: 6, name: "Oy Pohjolan Liikenne Ab" },
+  { id: 12, name: "Helsingin Bussiliikenne Oy" },
+  { id: 17, name: "Tammelundin Liikenne Oy" },
+  { id: 18, name: "Pohjolan Kaupunkiliikenne Oy" },
+  { id: 20, name: "Bus Travel Åbergin Linja Oy" },
+  { id: 21, name: "Bus Travel Oy Reissu Ruoti" },
+  { id: 22, name: "Nobina Finland Oy" },
+  { id: 30, name: "Savonlinja Oy" },
+  { id: 36, name: "Nurmijärven Linja Oy" },
+  { id: 40, name: "HKL-Raitioliikenne" },
+  { id: 45, name: "Transdev Vantaa Oy" },
+  { id: 47, name: "Taksikuljetus Oy" },
+  { id: 50, name: "HKL-Metroliikenne" },
+  { id: 51, name: "Korsisaari Oy" },
+  { id: 54, name: "V-S Bussipalvelut Oy" },
+  { id: 55, name: "Transdev Helsinki Oy" },
+  { id: 58, name: "Koillisen Liikennepalvelut Oy" },
+  { id: 60, name: "Suomenlinnan Liikenne Oy" },
+  { id: 59, name: "Tilausliikenne Nikkanen Oy" },
+  { id: 89, name: "Metropolia" },
+  { id: 90, name: "VR Oy" },
+];
