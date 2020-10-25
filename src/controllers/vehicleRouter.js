b7fc0f38f4ec0fb95c8e4edbad0cb9a746016ec5
Jouni Männistö <jouni.mannisto@gmail.com>
2020-10-25 19:23:12 +0200

    Update vehicleRouter.js
    Data model updates

--- src/controllers/vehicleRouter.js
+++ src/controllers/vehicleRouter.js
@@ -9,19 +9,19 @@ vehicleRouter.get('/', async (request, response) => {
 vehicleRouter.post('/', async (request, response) => {
     const body = request.body;
     const vehicleType = body.type;
-    // todo selectVehicleObj
+    
     const result = connectTo(vehicleType);
     // 202 Accepted
     response.status(202).json(result);
 });
 
 const vehicleTypes = [
-  { type: "tram" },
-  { type: "train" },
-  { type: "bus" },
-  { type: "metro" },
-  { type: "ferry" },
-  { type: "ubus" }
+  { type: "tram", icon: "tram" },
+  { type: "train", icon: "directions_railway" },
+  { type: "bus", icon: "directions_bus" },
+  { type: "metro", icon: "directions_subway" },
+  { type: "ferry", icon: "directions_boat" },
+  // { type: "ubus", icon: "directions_railway" },
 ];
 
 module.exports = vehicleRouter;
